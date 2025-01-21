32a08047137d0cc404cfb1af1d745c49a82d258f
Javierlr <javierlr85@educastur.es>
2025-01-21 13:21:04 +0100

    21/01

---
+++ Documents/NetBeansProjects/Biblioteca/src/main/java/es/educastur/javierlr/biblioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.educastur.javierlr.biblioteca;
+
+/**
+ *
+ * @author alu14d
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
